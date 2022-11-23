6aa0d3d66792dd61b4fa41db23482f2f47849250
BADAH Bilal <bilalbadah@gmail.com>
2022-11-23 19:50:52 +0100

    Create Project and setup the solution

---
+++ First Test with Identity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using First_Test_with_Identity.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(First_Test_with_Identity.Areas.Identity.IdentityHostingStartup))]
+namespace First_Test_with_Identity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
