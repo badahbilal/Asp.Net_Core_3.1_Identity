54c54984d9f44c4ceee777472ea167adf94f6cba
BADAH Bilal <bilalbadah@gmail.com>
2022-11-23 21:21:31 +0100

    Episode 2 : Change the tables names with fluent api

--- First Test with Identity/Data/ApplicationDbContext.cs
+++ First Test with Identity/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -12,5 +13,21 @@ namespace First_Test_with_Identity.Data
             : base(options)
         {
         }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser>().ToTable("Users", "security");
+            builder.Entity<IdentityRole>().ToTable("Role", "security");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRoles", "security");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims", "security");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins", "security");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims", "security"); 
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens", "security");
+
+        }
+
     }
 }
--- First Test with Identity/Data/Migrations/20221123201301_RenameIdentityTable.Designer.cs
+++ First Test with Identity/Data/Migrations/20221123201301_RenameIdentityTable.Designer.cs
@@ -4,14 +4,16 @@ using First_Test_with_Identity.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace First_Test_with_Identity.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221123201301_RenameIdentityTable")]
+    partial class RenameIdentityTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
